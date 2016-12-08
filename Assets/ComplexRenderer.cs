0fcf393cf928d0a2502845e980e65e311f579cd8
Erhannis Kirran <eyeillus@gmail.com>
2016-12-08 08:34:25 -0500

    Made a renderer for lines of complex numbers

---
+++ Assets/ComplexRenderer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Numerics; // Illusion; copied in Complex.cs
+
+public class ComplexRenderer : MonoBehaviour {
+    public LineRenderer lineRenderer;
+    private int count;
+    private Complex[] values;
+    private Vector3[] positions;
+
+	// Use this for initialization
+	void Start () {
+        count = -1;
+	}
+
+    private void UpdatePositions() {
+        for (int i = 0; i < count; i++) {
+            Complex val = values[i];
+            positions[i] = new Vector3((float)val.Real, (float)val.Imaginary, (((float)i) / count));
+        }
+    }
+
+    private void UpdateLine() {
+        lineRenderer.SetPositions(positions);
+    }
+
+    /**
+     * If you change the numbers in values after calling this, the lines will not automatically
+     * update, fyi.
+     */
+    public void SetValues(Complex[] values) {
+        this.values = values;
+        if (values.Length != count) {
+            count = values.Length;
+            positions = new Vector3[count];
+        }
+        lineRenderer.SetVertexCount(count);
+        UpdatePositions();
+        UpdateLine();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Numerics;
+
+public class Test : MonoBehaviour {
+    public ComplexRenderer complexRenderer;
+
+	// Use this for initialization
+	void Start () {
+        int count = 100;
+        Complex[] values = new Complex[count];
+        for (int i = 0; i < count; i++) {
+            values[i] = Complex.FromPolarCoordinates(1, i * 2 * Mathf.PI / count);
+        }
+
+        complexRenderer.SetValues(values);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float time = Time.realtimeSinceStartup;
+        int count = 100;
+        Complex[] values = new Complex[count];
+        for (int i = 0; i < count; i++) {
+            values[i] = Complex.FromPolarCoordinates(1, time + (i * 2 * Mathf.PI / count));
+        }
+
+        complexRenderer.SetValues(values);
+    }
+}
